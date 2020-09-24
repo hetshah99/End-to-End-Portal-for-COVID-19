c50df4e90c282abee554de772521f958e9beaea1
hetshah99 <hetshah25@gmail.com>
2020-09-24 09:41:53 +0530

    initital commit

---
+++ chatbot using ann/chatgui.py
@@ -0,0 +1,104 @@
+import nltk
+from nltk.stem import WordNetLemmatizer
+lemmatizer = WordNetLemmatizer()
+import pickle
+import numpy as np
+
+from keras.models import load_model
+model = load_model('chatbot_model.h5')
+import json
+import random
+intents = json.loads(open('intents.json',encoding="utf8").read())
+words = pickle.load(open('words.pkl','rb'))
+classes = pickle.load(open('classes.pkl','rb'))
+
+
+def clean_up_sentence(sentence):
+    sentence_words = nltk.word_tokenize(sentence)
+    sentence_words = [lemmatizer.lemmatize(word.lower()) for word in sentence_words]
+    return sentence_words
+
+
+def bow(sentence, words, show_details=True):
+    sentence_words = clean_up_sentence(sentence)
+    bag = [0]*len(words)
+    for s in sentence_words:
+        for i,w in enumerate(words):
+            if w == s:
+                bag[i] = 1
+                if show_details:
+                    print ("found in bag: %s" % w)
+    return(np.array(bag))
+
+def predict_class(sentence, model):
+    p = bow(sentence, words,show_details=False)
+    res = model.predict(np.array([p]))[0]
+    ERROR_THRESHOLD = 0.25
+    results = [[i,r] for i,r in enumerate(res) if r>ERROR_THRESHOLD]
+    results.sort(key=lambda x: x[1], reverse=True)
+    return_list = []
+    for r in results:
+        return_list.append({"intent": classes[r[0]], "probability": str(r[1])})
+    return return_list
+
+def getResponse(ints, intents_json):
+    tag = ints[0]['intent']
+    list_of_intents = intents_json['intents']
+    for i in list_of_intents:
+        if(i['tag']== tag):
+            result = random.choice(i['responses'])
+            break
+    return result
+
+def chatbot_response(msg):
+    ints = predict_class(msg, model)
+    res = getResponse(ints, intents)
+    return res
+
+
+import tkinter
+from tkinter import *
+
+
+def send():
+    msg = EntryBox.get("1.0",'end-1c').strip()
+    EntryBox.delete("0.0",END)
+
+    if msg != '':
+        ChatLog.config(state=NORMAL)
+        ChatLog.insert(END, "You: " + msg + '\n\n')
+        ChatLog.config(foreground="#430d27", font=("Verdana", 12 ))
+
+        res = chatbot_response(msg)
+        ChatLog.insert(END, "Bot: " + res + '\n\n')
+
+        ChatLog.config(state=DISABLED)
+        ChatLog.yview(END)
+
+
+base = Tk()
+base.title("COVID-19 Help Bot-We Answer all your questions")
+base.geometry("550x500")
+base.resizable(width=FALSE, height=FALSE)
+
+ChatLog = Text(base, bd=0, bg="#e2f3f5", height="8", width="50", font="Arial",)
+
+ChatLog.config(state=DISABLED)
+scrollbar = Scrollbar(base, command=ChatLog.yview)
+ChatLog['yscrollcommand'] = scrollbar.set
+
+SendButton = Button(base, font=("Verdana",12,'bold'), text="Send", width="12", height=5,
+                    bd=0, bg="#22d1ee", activebackground="#21e6c1",fg='#ffffff',
+                    command= send )
+
+EntryBox = Text(base, bd=0, bg="white",width="29", height="4", font="Arial")
+
+
+scrollbar.place(x=536,y=6, height=407)
+ChatLog.place(x=6,y=6, height=407, width=532)
+EntryBox.place(x=6, y=422, height=72, width=404)
+SendButton.place(x=416, y=422, height=72)
+
+
+
+base.mainloop()
